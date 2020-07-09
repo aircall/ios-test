1e3a7dffd3907080dbab075cc58722ac18335ce7
pjechris <pjechris@users.noreply.github.com>
2020-07-09 11:54:09 +0200

    project init

---
+++ Aircall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aircall
+//
+//  Created by JC on 09/07/2020.
+//  Copyright © 2020 JC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
