0726712e1c70dc7efce89f6fe0256fb9de3abb96
pjechris <pjechris@users.noreply.github.com>
2020-07-09 13:20:45 +0200

    [domain] Call model

---
+++ Aircall/Domain/Call/Call.swift
@@ -0,0 +1,43 @@
+//
+//  Call.swift
+//  Aircall
+//
+//  Created by JC on 09/07/2020.
+//  Copyright © 2020 JC. All rights reserved.
+//
+
+import Foundation
+
+/// In real-app we would parse and make a data struct instead of String to access phone number info
+/// (like international code) and custom format
+typealias PhoneNumber = String
+
+struct Call: Identifiable {
+    let id: Int
+    let createdAt: Date
+    let direction: Direction
+    let from: Caller
+    let to: Caller?
+    let via: String
+    let duration: Duration
+    let isArchived: Bool
+    let callType: Status
+}
+
+extension Call {
+    enum Direction: String {
+        case inbound
+        case outbound
+    }
+    
+    enum Status: String {
+        case missed
+        case answered
+        case voicemail
+    }
+}
+
+enum Caller {
+    case phone(number: PhoneNumber)
+    case contact(name: String)
+}
---
+++ Aircall/Domain/Duration.swift
@@ -0,0 +1,13 @@
+//
+//  Duration.swift
+//  Aircall
+//
+//  Created by JC on 09/07/2020.
+//  Copyright © 2020 JC. All rights reserved.
+//
+
+import Foundation
+
+enum Duration {
+    case seconds(Int)
+}
