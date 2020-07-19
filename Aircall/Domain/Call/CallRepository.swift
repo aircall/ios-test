0e4e686abc1a458aa94e9d515134e847d8bea7ba
pjechris <pjechris@users.noreply.github.com>
2020-07-19 16:07:09 +0200

    [domain] call api

---
+++ Aircall/Domain/Call/CallRepository.swift
@@ -0,0 +1,15 @@
+//
+//  CallRepository.swift
+//  Aircall
+//
+//  Created by JC on 19/07/2020.
+//  Copyright © 2020 JC. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+protocol CallRepository {
+    func loadAll() -> Future<[Call], Error>
+    func save(call: Call) -> Future<Call, Error>
+}
