3c01a9cf460a09d32eda9397e41495973d79dce4
Laurent Noudohounsi <11269572+laurentNoudohounsi@users.noreply.github.com>
2021-04-25 21:57:25 +0200

    Add Themeable protocol

---
+++ AircallLaurent/AircallLaurent/Shared/Protocol/Xib/Themeable.swift
@@ -0,0 +1,19 @@
+//
+//  Themeable.swift
+//  AircallLaurent
+//
+//  Created by Laurent on 25/04/2021.
+//
+
+import UIKit
+
+/*******************************************************************************
+ * Reusable
+ *
+ * A type that can have a visual theme applied.
+ *
+ ******************************************************************************/
+
+protocol Themeable {
+  var themeColor: UIColor { get set }
+}
