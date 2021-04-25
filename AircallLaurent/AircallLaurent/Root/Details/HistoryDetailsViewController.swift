ec1495dc3b4e9cd9f6d9ee3a7a827e71650234cf
Laurent Noudohounsi <11269572+laurentNoudohounsi@users.noreply.github.com>
2021-04-25 22:22:45 +0200

    Add HistoryDetailsViewController viewModel

--- AircallLaurent/AircallLaurent/Root/Details/HistoryDetailsViewController.swift
+++ AircallLaurent/AircallLaurent/Root/Details/HistoryDetailsViewController.swift
@@ -18,6 +18,16 @@ class HistoryDetailsViewController: UIViewController {
   @IBOutlet weak private var InformationContainerView: UIView!
   @IBOutlet weak private var ActionContainerView: UIView!
 
+  /******************** ViewModel ********************/
+
+  private let viewModel: HistoryDetailsViewModel
+
+  /******************** ViewControllers ********************/
+
+  private lazy var actionViewController: HistoryDetailsActionViewController = {
+    return HistoryDetailsActionViewController(nibName: nil, bundle: nil)
+  }()
+
   //----------------------------------------------------------------------------
   // MARK: - Lifecycle
   //----------------------------------------------------------------------------
@@ -31,8 +41,51 @@ class HistoryDetailsViewController: UIViewController {
   // MARK: - Initialization
   //----------------------------------------------------------------------------
 
-  private func setup() {
+  // Deactivate all the init except the `init(with:)` and avoid to add a
+  // dependancy injection library.
 
+  @available(*, unavailable)
+  convenience init() {
+    fatalError("init() has not been implemented.")
+  }
+
+  @available(*, unavailable)
+  override init(nibName nibNameOrNil: String?,
+                bundle nibBundleOrNil: Bundle?) {
+    fatalError("init(nibNameOrNil:nibBundleOrNil:) has not been implemented.")
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented.")
+  }
+
+  init(with viewModel: HistoryDetailsViewModel) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  private func setup() {
+    setupView()
+    setupDetailsActionView()
+  }
+
+  private func setupView() {
+
+  }
+
+  private func setupDetailsActionView() {
+    actionViewController.shouldAddNote = { [weak self] in
+      self?.viewModel.addNote()
+    }
+
+    actionViewController.shouldAddTags = { [weak self] in
+      self?.viewModel.tag()
+    }
+
+    actionViewController.shouldAssign = { [weak self] in
+      self?.viewModel.assign()
+    }
   }
   
 }
