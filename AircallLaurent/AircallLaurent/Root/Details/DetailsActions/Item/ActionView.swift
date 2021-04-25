39063b1cc941e2f16d4e509ebc7ba97453f4103c
Laurent Noudohounsi <11269572+laurentNoudohounsi@users.noreply.github.com>
2021-04-25 21:50:05 +0200

    Add ActionView class

---
+++ AircallLaurent/AircallLaurent/Root/Details/DetailsActions/Item/ActionView.swift
@@ -0,0 +1,99 @@
+//
+//  ActionView.swift
+//  AircallLaurent
+//
+//  Created by Laurent on 25/04/2021.
+//
+
+import UIKit
+
+/*******************************************************************************
+ * CollectionV2MovementView
+ *
+ * An item used inside an alert / actioSheet view o
+ *
+ ******************************************************************************/
+
+class ActionView: UIView, NibInstanciable {
+
+  //----------------------------------------------------------------------------
+  // MARK: - Properties
+  //----------------------------------------------------------------------------
+
+  /******************** Outlets ********************/
+
+  @IBOutlet var contentView: UIView!
+  @IBOutlet weak private var backgroundView: UIView!
+  @IBOutlet weak private var imageView: UIImageView!
+  @IBOutlet weak private var textLabel: UILabel!
+
+  /******************** Labels ********************/
+
+  /// Image of the view
+  @IBInspectable var image: UIImage? {
+    get { return imageView.image }
+    set { imageView.image = newValue }
+  }
+
+  /// Tile of the view
+  @IBInspectable var titleText: String? {
+    get { return textLabel.text }
+    set { textLabel.text = newValue }
+  }
+
+  /******************** Callbacks ********************/
+
+  var didTap: (() -> Void)?
+
+  //----------------------------------------------------------------------------
+  // MARK: - Initialization
+  //----------------------------------------------------------------------------
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    setup()
+  }
+
+  private func setup() {
+    loadNib()
+    setupView()
+    setupContentView()
+    setupImage()
+    setupTitle()
+  }
+
+  private func setupView() {
+    backgroundColor = .clear
+  }
+
+  private func setupBackgroundView() {
+    backgroundView.backgroundColor = .white
+  }
+
+  private func setupContentView() {
+    contentView.backgroundColor = .clear
+    add(subview: contentView, with: .edge)
+  }
+
+  private func setupImage() {
+    imageView.image = nil
+  }
+
+  private func setupTitle() {
+    textLabel.numberOfLines = 1
+  }
+
+  //----------------------------------------------------------------------------
+  // MARK: - Action
+  //----------------------------------------------------------------------------
+
+  @IBAction func tap(_ sender: Any) {
+    didTap?()
+  }
+
+}
