15ca2249c8b4919972c9c9b39d7dd59db59b766f
Laurent Noudohounsi <11269572+laurentNoudohounsi@users.noreply.github.com>
2021-04-26 18:09:26 +0200

    Add date converter and tests

---
+++ AircallLaurent/AircallLaurent/Shared/extension/Date/Date+Converter.swift
@@ -0,0 +1,36 @@
+//
+//  Date+Converter.swift
+//  AircallLaurent
+//
+//  Created by Laurent on 26/04/2021.
+//
+
+import Foundation
+
+extension Date {
+
+  init?(fromISO8601 iso8601Date: String) {
+    // TODO: Localize day and time. Out of scope for this test.
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.000Z"
+    guard let date = dateFormatter.date(from: iso8601Date) else {
+      return nil
+    }
+    self = date
+  }
+
+  func timeIn24HourFormat() -> String {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .none
+    formatter.dateFormat = "HH:mm"
+    return formatter.string(from: self)
+  }
+
+  func monthDayShortFormat() -> String {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .none
+    formatter.dateFormat = "MMM. dd"
+    return formatter.string(from: self)
+  }
+
+}
---
+++ AircallLaurent/AircallLaurentTests/Date/DateTests.swift
@@ -0,0 +1,44 @@
+//
+//  DateTests.swift
+//  AircallLaurentTests
+//
+//  Created by Laurent on 26/04/2021.
+//
+
+import XCTest
+
+class DateTests: XCTestCase {
+
+  func test_iso8601_to_date() throws {
+    let iso8601Date = "2018-04-18T16:59:48.000Z"
+    guard let date = Date(fromISO8601: iso8601Date) else {
+      XCTFail("Unable to convert iso8601 date to Date")
+      return
+    }
+
+    let expectedTimestamp: TimeInterval = 1524070788
+
+    XCTAssert(date.timeIntervalSince1970 == expectedTimestamp)
+  }
+
+  func test_date_to_time_string() throws {
+    let timeStamp: TimeInterval = 1524070788
+
+    let date = Date(timeIntervalSince1970: timeStamp)
+    let resultTime = date.timeIn24HourFormat()
+
+    let expectedTime = "18:59"
+    XCTAssert(resultTime == expectedTime)
+  }
+
+  func test_date_to_day_string() throws {
+    let timeStamp: TimeInterval = 1524070788
+
+    let date = Date(timeIntervalSince1970: timeStamp)
+    let resultMonthDay = date.monthDayShortFormat()
+
+    let expectedMonthDay = "Apr. 18"
+    XCTAssert(resultMonthDay == expectedMonthDay)
+  }
+
+}
