b693ba2c0a01cfe76f3ee6072816880c22e770e1
Laurent Noudohounsi <11269572+laurentNoudohounsi@users.noreply.github.com>
2021-04-27 10:31:50 +0200

    Deactivate HistoryDetailsContentViewController scroll

--- AircallLaurent/AircallLaurent/Root/Details/Content/HistoryDetailsContentViewController.swift
+++ AircallLaurent/AircallLaurent/Root/Details/Content/HistoryDetailsContentViewController.swift
@@ -26,7 +26,8 @@ class HistoryDetailsContentViewController: UIViewController {
   private let tableViewDataSource:
     HistoryDetailsContentDataSource
 
-  private let tableViewDelegate: TableViewDelegate<HistoryDetailsContentDataProvider>
+  private let tableViewDelegate:
+    TableViewDelegate<HistoryDetailsContentDataProvider>
 
   /******************** Callbacks ********************/
 
@@ -37,8 +38,8 @@ class HistoryDetailsContentViewController: UIViewController {
   //--------------------------------------------------------------------------
 
   override func viewDidLoad() {
-      super.viewDidLoad()
-      setup()
+    super.viewDidLoad()
+    setup()
   }
 
   //--------------------------------------------------------------------------
@@ -47,88 +48,89 @@ class HistoryDetailsContentViewController: UIViewController {
 
   @available(*, unavailable)
   convenience init() {
-      fatalError("init() has not been implemented.")
+    fatalError("init() has not been implemented.")
   }
 
   @available(*, unavailable)
   override init(nibName nibNameOrNil: String?,
                 bundle nibBundleOrNil: Bundle?) {
-      fatalError(
-          "init(nibNameOrNil:nibBundleOrNil:) has not been implemented."
-      )
+    fatalError(
+      "init(nibNameOrNil:nibBundleOrNil:) has not been implemented."
+    )
   }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
-      fatalError("init(coder:) has not been implemented.")
+    fatalError("init(coder:) has not been implemented.")
   }
 
   init(with dataProvider: HistoryDetailsContentDataProvider) {
-      self.dataProvider = dataProvider
-      tableViewDataSource = HistoryDetailsContentDataSource(with: dataProvider)
-      tableViewDelegate = TableViewDelegate<HistoryDetailsContentDataProvider>(
-        with: dataProvider
-      )
+    self.dataProvider = dataProvider
+    tableViewDataSource = HistoryDetailsContentDataSource(with: dataProvider)
+    tableViewDelegate = TableViewDelegate<HistoryDetailsContentDataProvider>(
+      with: dataProvider
+    )
 
-      super.init(nibName: nil, bundle: .main)
+    super.init(nibName: nil, bundle: .main)
   }
 
   private func setup() {
-      setupView()
-      setupDataProvider()
-      setupTableView()
+    setupView()
+    setupDataProvider()
+    setupTableView()
   }
 
   private func setupView() {
-      view.backgroundColor = .clear
+    view.backgroundColor = .clear
   }
 
 
   private func setupDataProvider() {
-      dataProvider.dataDidChange = { [weak self] in
-          guard let self = self else { return }
-          self.dataProvider.registerOn(self.tableView)
-          self.tableView.reloadData()
-      }
+    dataProvider.dataDidChange = { [weak self] in
+      guard let self = self else { return }
+      self.dataProvider.registerOn(self.tableView)
+      self.tableView.reloadData()
+    }
 
-      dataProvider.dataDidClear = { [weak self] in
-          self?.tableView.reloadData()
-      }
+    dataProvider.dataDidClear = { [weak self] in
+      self?.tableView.reloadData()
+    }
 
-      dataProvider.registerOn(tableView)
+    dataProvider.registerOn(tableView)
   }
 
   private func setupTableView() {
-      setupTableViewStyle()
-      setupTableViewDataSource()
-      setupTableViewDelegate()
+    setupTableViewStyle()
+    setupTableViewDataSource()
+    setupTableViewDelegate()
   }
 
   private func setupTableViewStyle() {
-      tableView.backgroundColor = .clear
-      tableView.separatorStyle = .none
-      tableView.showsHorizontalScrollIndicator = false
-      tableView.showsVerticalScrollIndicator = false
-      tableView.bounces = true
+    tableView.backgroundColor = .clear
+    tableView.separatorStyle = .none
+    tableView.showsHorizontalScrollIndicator = false
+    tableView.showsVerticalScrollIndicator = false
+    tableView.bounces = false
+    tableView.isScrollEnabled = false
 
-      tableView.estimatedRowHeight = 74.0
-      tableView.rowHeight = UITableView.automaticDimension
+    tableView.estimatedRowHeight = 74.0
+    tableView.rowHeight = UITableView.automaticDimension
   }
 
   private func setupTableViewDataSource() {
-      tableView.dataSource = tableViewDataSource
+    tableView.dataSource = tableViewDataSource
   }
 
   private func setupTableViewDelegate() {
-      tableView.delegate = tableViewDelegate
+    tableView.delegate = tableViewDelegate
   }
 
   //--------------------------------------------------------------------------
   // MARK: - Update
   //--------------------------------------------------------------------------
 
-//  func update(from call: CallModel) {
-//      dataProvider.updateData(with: list)
-//  }
+  //  func update(from call: CallModel) {
+  //      dataProvider.updateData(with: list)
+  //  }
 
 }
